a94f290a186567930ed9360cab084f72655efb0f
Luis Cabrera Benito <contacto@parzibyte.me>
2019-02-04 13:39:37 -0600

    Cambiado a página de inicio

--- index.php
+++ index.php
@@ -1,24 +1,28 @@
 <?php
-/*
-    Combinar documentos PDF con PHP
-    y libmergepdf
+?>
+<h1>Combinar PDF's</h1>
+<p>
+Ejemplo demostrativo de combinación de documentos PDF. 
+<a href="https://parzibyte.me/blog/2019/02/04/unir-combinar-archivos-pdf-php-libmergepdf/">Mira el tutorial</a>
+<br>
 
-    @author parzibyte
- */
-# Cargar librerías
-require_once "vendor/autoload.php";
-use iio\libmergepdf\Merger;
+<br>
+Ahora puedes visitar lo siguiente:
+<br>
+<a href="./mostrar_en_navegador.php">Mostrar PDF combinado en navegador</a>
+<br>
+<a href="./descargar.php">Forzar descarga de PDF</a>
+<br>
+<a href="./guardar.php">Guardar en disco duro</a> (mira la <a href="./combinado.pdf">salida</a> después de haberlo guardado)
+<br><br>
+Por cierto, aquí los documentos utilizados:
 
-# Ruta de los documentos
-$documentos = ["cotizacion.pdf", "parzibyte.pdf", "documento.pdf"];
-
-# Crear el "combinador"
-$combinador = new Merger;
-
-# Agregar archivo en cada iteración
-foreach ($documentos as $documento) {
-    $combinador->addFile($documento);
-}
-
-# Y combinar o unir
-$salida = $combinador->merge();
+<br>
+<a href="./parzibyte.pdf">parzibyte.pdf</a>
+<br>
+<a href="./cotizacion.pdf">cotizacion.pdf</a>
+<br>
+<a href="./documento.pdf">documento.pdf</a>
+<br>
+by <a href="https://parzibyte.me">parzibyte</a>
+</p>
